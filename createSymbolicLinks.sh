d00ca9939a7c6fadad6ce88de3a55f7d68228c71
ShubhDPatel <shubh09226@gmail.com>
2025-01-03 01:12:41 -0500

    Add starship prompt to .sh

--- createSymbolicLinks.sh
+++ createSymbolicLinks.sh
@@ -37,3 +37,22 @@ else
         echo "Failed to create symbolic link for nvim."
     fi
 fi
+
+# Create symlink for starship.toml
+SOURCE_STARSHIP="$HOME/.dotfiles/starship.toml"
+DESTINATION_STARSHIP="$HOME/.config/starship.toml"
+
+# Check if the starship.toml symlink destination already exists
+if [ -e "$DESTINATION_STARSHIP" ]; then
+    echo "Error: $DESTINATION_STARSHIP already exists. Remove it first if you want to recreate the symlink."
+else
+    # Create the symbolic link for starship.toml
+    ln -s "$SOURCE_STARSHIP" "$DESTINATION_STARSHIP"
+
+    # Verify the link creation for starship.toml
+    if [ -L "$DESTINATION_STARSHIP" ]; then
+        echo "Symbolic link created for starship.toml: $DESTINATION_STARSHIP -> $SOURCE_STARSHIP"
+    else
+        echo "Failed to create symbolic link for starship.toml."
+    fi
+fi
