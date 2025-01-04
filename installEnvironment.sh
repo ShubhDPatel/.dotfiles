f46d2c30955e3e20eb5c4285d9397a88068b0296
ShubhDPatel <shubh09226@gmail.com>
2025-01-04 00:15:10 -0500

    Add starship to .sh

--- installEnvironment.sh
+++ installEnvironment.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+install_starship() {
+    echo "Checking if Starship is already installed..."
+    if command -v starship &> /dev/null; then
+        echo "Starship is already installed. Skipping installation."
+    else
+        echo "Installing Starship prompt manager..."
+        if curl -sS https://starship.rs/install.sh | sh; then
+            echo "Starship installation succeeded."
+        else
+            echo "Starship installation failed."
+        fi
+  fi
+}
+
+install_starship
